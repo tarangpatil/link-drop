25ffab3c0a63bb7742bd892151a216f6db8aec08
Tarang Patil <tarangpatil09@gmail.com>
2026-01-17 12:22:25 +0530

    Setup Prisma ORM alongwith bcrypt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <main></main>;
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcrypt";
+
+export default async function Home() {
+  const id = 1;
+  const name = "Tarang Patil";
+  const email = "turupatil@linkdrop.com";
+  const password = await bcrypt.hash("very-secure-password", 12);
+
+  await prisma.user.deleteMany();
+  const turuPatil = await prisma.user.create({
+    data: { id, name, email, password },
+  });
+
+  return (
+    <main className="container">
+      <h1>Hi, {turuPatil.name}</h1>
+      <code>{JSON.stringify(turuPatil)}</code>
+    </main>
+  );
 }
